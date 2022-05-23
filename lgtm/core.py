9de7b5f8da29603f62089cd13281096faf329a7b
sasahara <sasahiro1204@icloud.com>
2022-05-23 23:22:03 +0900

    fin

--- lgtm/core.py
+++ lgtm/core.py
@@ -1,15 +1,18 @@
 #https://blog.amedama.jp/entry/2015/10/14/232045
 import click
 
+from lgtm.drawer import save_with_message
+from lgtm.image_source import get_image
+
 @click.command()
 @click.option('--message', '-m', default='LGTM',
               show_default=True, help='画像に乗せる文字列')
 @click.argument('keyword')
-def cli():
-    """LGTM画像作成ツール"""
+def cli(keyword, message):
+    """LGTM画像生成ツール"""
     lgtm(keyword, message)
-    click.echo('lgtm')
 
-def lgtm():
-    #ここにロジックを追加していく
-    pass
+
+def lgtm(keyword, message):
+    with get_image(keyword) as fp:
+        save_with_message(fp, message)
